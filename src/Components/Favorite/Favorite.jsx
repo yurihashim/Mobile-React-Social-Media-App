dfb76ede657e9d314fda6fd27ff051866bc831e8
Yuki <yukipooh4120@gmail.com>
2021-06-20 12:18:12 -0700

    fixed favorite

--- src/Components/Favorite/Favorite.jsx
+++ src/Components/Favorite/Favorite.jsx
@@ -6,35 +6,49 @@ import FeedContext from "../../Contexts/FeedContext";
 const Favorite = () => {
   const { images } = useContext(FeedContext);
 
+  console.log(images);
+
   return (
     <>
       <div className="favoriteContainer">
-        <h2>Your Favorite Photo Collection</h2>
-        {images.favorite.length !== 0 ? (
-          <>
-            <Row className="topFavorite">
-              <Col className="left">
-                <img src={images.favorite[0].largeImageURL} alt="favorite1"></img>
-                <img src={images.favorite[1].largeImageURL} alt="favorite2"></img>
-              </Col>
-              <Col className="right">
-                <img src={images.favorite[2].largeImageURL} alt="favorite3"></img>
-              </Col>
-            </Row>
+        {images.favorite.length !== 0 ?
+          (images.favorite.length > 2 ?
+            (
+              <>
+                <h2>Your Favorite Photo Collection</h2>
+                <Row className="topFavorite">
+                  <Col className="left">
+                    <img src={images.favorite[0].largeImageURL} alt="favorite1"></img>
+                    <img src={images.favorite[1].largeImageURL} alt="favorite2"></img>
+                  </Col>
+                  <Col className="right">
+                    <img src={images.favorite[2].largeImageURL} alt="favorite3"></img>
+                  </Col>
+                </Row>
 
-            {/* layout change */}
-            {(() => {
-              const html = [];
-              for (let i = 3; i < images.favorite.length; i++) {
-                html.push(
-                  <Col className="col-4"><img src={images.favorite[i].largeImageURL} alt="favorite3"></img></Col>
-                );
-              }
-              return <Row className="bottomFavorite">{html}</Row>;
-            })()}
-
-          </>
-        ) : (<h2>No collection added yet</h2>)}
+                {/* layout change */}
+                {(() => {
+                  const html = [];
+                  for (let i = 3; i < images.favorite.length; i++) {
+                    html.push(
+                      <Col className="col-4"><img src={images.favorite[i].largeImageURL} alt="favorite3"></img></Col>
+                    );
+                  }
+                  return <Row className="bottomFavorite">{html}</Row>;
+                })()}
+              </>
+            ) : (
+              (() => {
+                const html = [];
+                for (let i = 0; i < images.favorite.length; i++) {
+                  html.push(
+                    <Col className="col-4"><img src={images.favorite[i].largeImageURL} alt="favorite3"></img></Col>
+                  );
+                }
+                return (<><h2>Your Favorite Photo Collection</h2><Row className="layout2">{html}</Row></>);
+              })()
+            ))
+          : (<h2>No collection added yet</h2>)}
 
       </div>
     </>
